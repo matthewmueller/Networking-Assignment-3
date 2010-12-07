579e48133f4d0a79b6fffc974bc351b9287b3c3b
MattMueller <mattmuelle@gmail.com>
2010-12-07 03:53:15 -0600

    Added helper file getMyIP.cpp. This will help for building the topology.txt.

---
+++ getMyIP.cpp
@@ -0,0 +1,12 @@
+#include "utilities.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main (int argc, char const *argv[])
+{
+	cout << "My IP is: " << getMyIP() << endl;
+	
+	return 0;
+}
