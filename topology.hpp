86120039849a4c57baef75011b6fe0416b253f0b
MattMueller <mattmuelle@gmail.com>
2010-12-09 01:33:01 -0600

    Fixed Topology and Node to keep offline all set up.

--- topology.hpp
+++ topology.hpp
@@ -21,24 +21,33 @@ public:
 			vector<string> nodes = explode(" ", lines.at(i));
 			
 			vector<string> node = explode(",", nodes.at(0));
-			Node me = Node().host(node[0]).port(atoi(node[1].c_str())).online(true);
+			Node n = Node().host(node[0]).port(atoi(node[1].c_str())).online(true);
+			this->add(n);
+			
+			Node &me = this->get(n);
 			
 			for(unsigned int j = 1; j < nodes.size(); j++) {
 				vector<string> node = explode(",", nodes.at(j));
-				Node n = Node().host(node[0]).port(atoi(node[1].c_str())).online(true);
-				me.neighbor(n);
+				// Node n = Node().host(node[0]).port(atoi(node[1].c_str())).online(true);
+				int p = atoi(node[1].c_str());
+				this->add(node[0], p);
+				Node &neighb = this->get(node[0], p);
+				
+				me.neighbor(neighb);
 			}
 			
-			this->add(me);
 		}
 	};	
 	
-	Node getNode(string host, int port) {
-		stringstream key;
-		key << host << ":" << port;
+	Node& get(Node n) {
+		return this->get(n.host(), n.port());
+	}
+	
+	Node& get(string host, int port) {
+		string key = this->getKey(host, port);
 				
-		if(_nodes.count(key.str())) {
-			return _nodes[key.str()];
+		if(_nodes.count(key)) {
+			return _nodes[key];
 		}
 		
 		cout << "Emulator node not present in topology table" << endl;
@@ -47,17 +56,76 @@ public:
 	
 	Topology& add(Node n) { 
 		string key = this->getKey(n);
-		_nodes[key] = n;
 		
+		if(!_nodes.count(key))
+			_nodes[key] = n;
+
+		return *this;
+	};
+	
+	Topology& add(string host, int port) { 
+		Node n = Node(host, port, 1);
+		this->add(n);
+
 		return *this;
 	};
 
+	Topology& disable(string host, int port) {
+		Node &n = this->get(host, port);
+		this->disable(n);
+		return *this;
+	}
+
 	Topology& disable(Node n) {
 		string key = this->getKey(n);
-		Node offline = _nodes[key];
-		offline.online(false);
+		// Pull key from mapping
+		Node &node = _nodes[key];
+
+		node.online(false);
+		//offline.online(false);
 		return *this;
 	};
+	
+	Topology& enable(Node n) {
+		string key = this->getKey(n);
+		Node &node = _nodes[key];
+		
+		node.online(true);
+		
+		return *this;
+	}
+		
+	string neighborsToString(Node n) {
+		stringstream out;
+		
+		vector<Node> _neighbors = n.neighbors();
+		
+		for(unsigned int i = 0; i < _neighbors.size(); i++) {
+			Node n = _neighbors.at(i);
+			n = this->get(n);
+			out << n.host() << ":" << n.port() << ":" << n.online();
+			out << ",";
+		}
+		string output = out.str();
+		
+		output.erase(output.size()-1);
+		
+		return output;
+	}
+	
+	// vector<Node> neighborsFromString(string neighbors) {
+	// 	vector<Node> out;
+	// 	
+	// 	vector<string> nodes = explode(",", neighbors);
+	// 	for(unsigned int i = 0; i < nodes.size(); i++) {
+	// 		string node = nodes.at(i);
+	// 		vector<string>pieces = explode(":", node);
+	// 		Node n = Node(pieces[0], atoi(pieces[1].c_str()), atoi(pieces[2].c_str()));
+	// 		out.push_back(n);
+	// 	}
+	// 	
+	// 	return out;
+	// }
 	
 	string print() {
 		stringstream out;
@@ -65,13 +133,36 @@ public:
 		
 		for(it = _nodes.begin(); it != _nodes.end(); ++it) {
 			Node n = it->second;
-			out <<  n.print() << endl;
+			n = this->get(n);
+			
+			out << "Node ( " << n.host() << " - " << n.port() << " - " << n.online() << " ) --> "; 
+			out << "{ ";
+			
+			vector<Node>neighbors = n.neighbors();
+			for(int unsigned i = 0; i < neighbors.size(); i++) {
+				Node neighbor = neighbors.at(i);
+				neighbor = this->get(neighbor);
+				out << "( " << neighbor.host() << " - " << neighbor.port() << " - " << neighbor.online() << " ) ";
+		
+				// if((it2)->first.compare(_neighbors.end()->first) == 0) break;
+		
+				out << ", ";
+			}
+			
+			out << " }" << endl;
+			
 		}
 		
 		return out.str();
 	};
 	
 private:
+	string getKey(string host, int port) {
+		stringstream key;
+		key << host << ":" << port;
+		return key.str();
+	}
+	
 	string getKey(Node n) {
 		stringstream key;
 		string host = n.host();
